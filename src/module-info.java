bea79097ab3479a6a6060c3bf5becfd2fedf3efa
DenRisk <35171305+DenRisk@users.noreply.github.com>
2019-04-03 17:41:56 +0200

    Initial commit

---
+++ src/module-info.java
@@ -0,0 +1,13 @@
+module LoLPicker {
+
+    requires javafx.web;
+    requires javafx.swing;
+    requires javafx.media;
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    requires javafx.base;
+    requires javafx.swt;
+
+    opens sample;
+}
