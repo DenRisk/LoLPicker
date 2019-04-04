3a972859e21343a30e42f2863a8f75c5b08768e7
DenRisk <DenRisk@users.noreply.github.com>
2019-04-04 10:52:48 +0200

    commit1

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,18 @@
 package sample;
 
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+
 public class Controller {
+
+    public ToggleButton toggletop;
+    public ToggleButton togglejungle;
+    public ToggleButton togglemid;
+    public ToggleButton toggleadc;
+    public ToggleButton togglesupp;
+
+    public ToggleGroup group;
+
+
+
 }
